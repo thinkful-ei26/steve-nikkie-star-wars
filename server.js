291c322db78dbb7e00a87b6ebee8b397dda0179a
Stephen Anderson <anderssj@usc.edu>
2018-11-15 10:16:41 -0700

    films working

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+'use strict'
 
-'use strict';
+const axios = require('axios')
 
-const axios = require('axios');
+//this will be the search term
+const searchTerm = process.argv[2]
 
-//this will be the search term 
-const searchTerm = process.argv[2];
-console.log(searchTerm);
-
-axios.get(`https://swapi.co/api/people/?search=${searchTerm}`).then(person=>{
-  // console.log(person);
-  console.log(person.data.results[0].name);
-});
+axios
+  .get(`https://swapi.co/api/people/?search=${searchTerm}`)
+  .then(person => {
+    // console.log(person);
+    console.log(person.data.results[0].name)
+    const url = person.data.results[0].films[0]
+    return axios.get(url)
+  })
+  .then(film => {
+    console.log(film.data.title)
+  })
